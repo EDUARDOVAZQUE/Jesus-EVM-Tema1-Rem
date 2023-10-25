ceb8b77b5fbeb5e7d374f9821533aa4316505e3d
EDUARDOVAZQUE <148374770+EDUARDOVAZQUE@users.noreply.github.com>
2023-10-25 10:54:27 -0600

    Delete Documents/DSA/Test1_JesusEVM_REMEDIAL directory

--- Documents/DSA/Test1_JesusEVM_REMEDIAL/ejercicio3.js
+++
@@ -1,6 +0,0 @@
-function ejercicio3([L1,L2,L3]){
-    p = (L1+L2+L3)/2
-    a = Math.sqrt(p*(p-L1)*(p-L2)*(p-L3))
-    return a.toFixed(2)
-}
-// console.log(ejercicio3([5,6,7]))
--- Documents/DSA/Test1_JesusEVM_REMEDIAL/ejercicio4.js
+++
@@ -1,9 +0,0 @@
-function ejercicio4(sueldo){
-    if(sueldo<1000){
-        nuevo_sueldo = sueldo*1.15
-    } else{
-        nuevo_sueldo = sueldo*1.12
-    }
-    return nuevo_sueldo.toFixed(2)
-}
-// console.log(ejercicio4(999))
